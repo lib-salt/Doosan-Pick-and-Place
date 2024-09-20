43dd05ec084b8c760433d855960e2725e70cf482
lib-salt <130981325+lib-salt@users.noreply.github.com>
2024-09-20 12:04:00 +0100

    Update dsr_bringup2_gazebo.launch.py

--- dsr_bringup2/launch/dsr_bringup2_gazebo.launch.py
+++ dsr_bringup2/launch/dsr_bringup2_gazebo.launch.py
@@ -216,7 +216,7 @@ def generate_launch_description():
         gazebo_connection_node,
         control_node,
         robot_state_pub_node,
-        robot_controller_spawner,
+        # robot_controller_spawner,
         delay_rviz_after_joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
         included_launch_after_robot_controller_spawner
